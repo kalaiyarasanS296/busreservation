e640c3131a2f83f5413748745b94316ed4003161
kalaiyarasanS296 <126502284+kalaiyarasanS296@users.noreply.github.com>
2024-07-10 22:01:52 +0530

    Add files via upload

---
+++ FibonacciSeriemais.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FibonacciSeriemais {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        int firstTerm = 0, secondTerm = 1;
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + ", ");
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
